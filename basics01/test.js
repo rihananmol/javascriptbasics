1db418f4e353557076d8a79971d80957d14b0157
rihananmol <jaspreetsingh49287@gmail.com>
2025-11-28 10:28:04 +0000

    changes

--- basics01/test.js
+++ basics01/test.js
@@ -1,2 +1,28 @@
-console.log("java")
-crossOriginIsolated
+/*const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+const accountCity = "chennai"
+console.log(accountId);
+
+// in accontCity the type of the variable is not defined so the program stops before using the const accontCity = "chennai"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState]) //
+// console.log(accountCity) //
+let danny = 22
+var danny = 33
+console.log(danny)
+// let and const are block scoped , while var are funtion scope and hoisted 
