2f9800528d43b54379092d51187f726ab5aec0db
rihananmol <jaspreetsingh49287@gmail.com>
2025-12-03 08:09:02 +0000

    cha

---
+++ basics01/con.js
@@ -0,0 +1,30 @@
+let score = "pardeep"
+//console.log(typeof score)
+// console.log(typeof(score))
+
+let valueinNumber = Number(score)
+//console.log(typeof valueinNumber)
+//console.log(valueinNumber)
+
+let num = ""
+//console.log(typeof num)
+//console.log(num)
+ 
+let say = 1
+//console.log(say)
+//console.log(typeof say)
+
+let raam = 1 
+let sachRam = Boolean(raam)
+//console.log(typeof sachRam)
+//console.log(sachRam)
+//let someNumber = 33 
+//let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+**************operations*****************************
+
+
+
+
