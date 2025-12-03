5ed21ad2103c7ed2a440ae2d8bf26ea90152b983
rihananmol <jaspreetsingh49287@gmail.com>
2025-12-03 09:41:47 +0000

    cjhh

--- basics01/con.js
+++ basics01/con.js
@@ -18,13 +18,52 @@ let raam = 1
 let sachRam = Boolean(raam)
 //console.log(typeof sachRam)
 //console.log(sachRam)
-//let someNumber = 33 
-//let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+let someNumber = 33 
+let stringNumber = String(someNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
 
-**************operations*****************************
+//**************operations*****************************
 
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+//console.log("1"=2="3") throws error
+console.log("1"+3+"2")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
 
 
 
